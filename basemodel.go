ec6c63f597c807765f22bca66055d4f6398f529d
Sergey Karp <ceo@tks.one>
2023-03-20 06:23:20 +0000

    Загрузить новый файл

---
+++ basemodel.go
@@ -0,0 +1,17 @@
+package appmodule
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type BaseModel struct {
+	Id        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
+	RemoveAt  time.Time `gorm:"default:null"`
+	GroupId   uuid.UUID `gorm:"default:0"`
+	CreatorId uuid.UUID
+	UpdaterId uuid.UUID
+	RemoverId uuid.UUID
+}
