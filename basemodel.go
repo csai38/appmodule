7f20806ac765d56911dacecfb56442883662ac51
skarp <ceo@tks.one>
2023-03-26 20:07:53 +0300

    Add response types and SysModel bagfix 1.0.5-003

--- basemodel.go
+++ basemodel.go
@@ -1,6 +1,7 @@
 package appmodule
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
@@ -10,23 +11,30 @@ type BaseModel struct {
 	Id        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
 	CreatedAt time.Time `gorm:"not null"`
 	UpdatedAt time.Time `gorm:"not null"`
-	RemoveAt  time.Time `gorm:"default:null"`
+	DeletedAt time.Time `gorm:"index;default:null"`
 	GroupId   uuid.UUID `gorm:"default:null"`
 	CreatorId uuid.UUID
 	UpdaterId uuid.UUID
-	RemoverId uuid.UUID
+	DeletedId uuid.UUID
 }
 
 type SysModules struct {
 	BaseModel
 	ModuleName string
 	Version    string
-	ApiMap     map[string][]map[string]string
+	ApiMap     string //Json module
+}
+
+type MethodParams struct {
+	Method      interface{}
+	Len         int
+	FormHandler bool
 }
 
 type BaseModule struct {
 	Db         *gorm.DB
-	ModuleMap  map[string]interface{}
+	ModuleName string
+	ModuleMap  map[string]MethodParams
 	ModuleDeps []map[string]string //format dependency key map - module name, value - version
 	Version    string              // module version format vX.X.X-YYY
 }
@@ -59,4 +67,31 @@ func (m BaseModule) ResponseTree(resError error, responseBody any, metaData any)
 		meta = metaData
 	}
 	return TreeResponse{true, responseBody, meta, errMsg}
+}
+
+func (m BaseModule) Api() map[string][]map[string]any {
+	api := map[string][]map[string]any{
+		m.ModuleName: {},
+	}
+	var el map[string]any
+	for k, v := range m.ModuleMap {
+		el = map[string]any{"name": k, "len": v.Len}
+		if v.FormHandler {
+			el["formHandler"] = true
+		}
+		api[m.ModuleName] = append(api[m.ModuleName], el)
+	}
+	return api
+}
+
+func (m BaseModule) Route(method string, args ...interface{}) (any, error) {
+	if val, ok := m.ModuleMap[method]; ok {
+		if len(args) == 0 {
+			return val.Method.(func() []map[string]string)(), nil
+		} else {
+			return val.Method.(func(string) []map[string]string)(args[0].(string)), nil
+		}
+	} else {
+		return nil, fmt.Errorf("Method %s not defined in module  %s", method, m.ModuleName)
+	}
 }
--- responsetypes.go
+++ responsetypes.go
@@ -1,23 +1,15 @@
 package appmodule
 
-type DirectResponse struct {
-	ReqType string         `json:"type"`
-	TId     int            `json:"tid"`
-	Action  string         `json:"action"`
-	Method  string         `json:"method"`
-	Result  map[string]any `json:"result"`
-}
-
 type DataResponse struct {
-	Success      bool   `json:"success"`
-	Data         any    `json:"data"`
-	MetaData     any    `json:"metaData"`
-	ErrorMessage string `json:"errorMessage"`
+	Success      bool   `json:"success,omitempty"`
+	Data         any    `json:"data,omitempty"`
+	MetaData     any    `json:"metaData,omitempty"`
+	ErrorMessage string `json:"errorMessage,omitempty"`
 }
 
 type TreeResponse struct {
-	Success      bool   `json:"success"`
-	Children     any    `json:"children"`
-	MetaData     any    `json:"metaData"`
-	ErrorMessage string `json:"errorMessage"`
+	Success      bool   `json:"success,omitempty"`
+	Children     any    `json:"children,omitempty"`
+	MetaData     any    `json:"metaData,omitempty"`
+	ErrorMessage string `json:"errorMessage,omitempty"`
 }
